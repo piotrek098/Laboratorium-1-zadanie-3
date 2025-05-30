597d9d194fc2137bbc1dd33df2c970c58dcdd66b
piotrek098 <cichockip3@gmail.com>
2025-05-30 21:01:46 +0200

    wejscia

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -20,7 +20,7 @@ class XORGenetyczny
         {
             new double[] { 0, 0, 0 },
             new double[] { 1, 0, 1 },
-            new double[] { 0, 1, 0 },
+            new double[] { 0, 1, 1 },
             new double[] { 1, 1, 0 }
         };
 
